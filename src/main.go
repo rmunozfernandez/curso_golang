8a574f68ad3c2beaeb387a9dce41fcbfa959c834
rmunozfernandez <rmunoz.it@gmail.com>
2022-12-25 21:22:34 -0300

    El poder de los ciclos en Golang: for, for while, for forever

--- src/main.go
+++ src/main.go
@@ -1,35 +1,28 @@
 package main
 
-func normalFunction(message string) {
-	println(message)
-}
-
-// buena practica: al declarar mismos tipos de dato, solo declararlo en el último dato
-func tripleArgument(a, b int, c string) {
-	println(a, b, c)
-}
-
-// retorna una valor int
-func returnValue(a int) int {
-	return a * 2
-}
-
-// retorcida la devolución de dos datos
-func dobleReturn(a int) (c, d int) {
-	return a, a * 2
-}
+import "fmt"
 
 func main() {
-	normalFunction("Hola mundo")
-	tripleArgument(1, 2, "Hola")
 
-	value := returnValue(2)
-	println("Value:", value)
+	// For condicional
+	fmt.Println("For condicionaal")
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
 
-	value1, value2 := dobleReturn(2)
-	println("value1 y value2", value1, value2)
+	// For while
+	fmt.Println("For while")
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
 
-	//cuando se devuelven varios datos de una función, se puede descartar usando el caracter _
-	value1, _ = dobleReturn(3)
-	println("value1: ", value1)
+	// For forever
+	fmt.Println("For forever")
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 }
