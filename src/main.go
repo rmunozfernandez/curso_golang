24d11d5988130c1f342fd4d1c408991c427feae6
rmunozfernandez <rmunoz.it@gmail.com>
2022-12-25 22:21:58 -0300

    Arrays y Slices

--- src/main.go
+++ src/main.go
@@ -3,25 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	// Defer
-	defer fmt.Println("Hola")
-	fmt.Println("mundo")
+	// Array - inmutables
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-	// Continue y break
-	for i := 0; i < 10; i++ {
+	// Slice - mutables
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-		// Continue
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
+	// Métodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+	//Nota: el primer digito es inclusivo y el segundo es exclusivo
 
-		// Break
-		if i == 8 {
-			fmt.Println("Break")
-			break
-		}
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
 
-		fmt.Println(i)
-	}
+	// Append nueva list
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...) // los 3 puntos significa que va a agregar cada elemento de la lista de forma independiente
+	fmt.Println(slice)
 }
