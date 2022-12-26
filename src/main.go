6fbe7c1327c00b9707ef7a0c849b6195935719f7
rmunozfernandez <rmunoz.it@gmail.com>
2022-12-25 23:01:28 -0300

    Recorrido For de Slice con Range

--- src/main.go
+++ src/main.go
@@ -2,30 +2,29 @@ package main
 
 import "fmt"
 
+func isPalindromo(text string) {
+	var textReverse string
+
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
+
+	if text == textReverse {
+		fmt.Println("Es palindromo")
+	} else {
+		fmt.Println("No es palindromo")
+	}
+}
+
 func main() {
-	// Array - inmutables
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array))
+	slice := []string{"hola", "que", "hace"}
+	for i, valor := range slice {
+		fmt.Println(i, valor)
+	}
 
-	// Slice - mutables
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
+	for _, valor := range slice {
+		fmt.Println(valor)
+	}
 
-	// Métodos en el slice
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
-	//Nota: el primer digito es inclusivo y el segundo es exclusivo
-
-	// Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
-
-	// Append nueva list
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...) // los 3 puntos significa que va a agregar cada elemento de la lista de forma independiente
-	fmt.Println(slice)
+	isPalindromo("amor a roma")
 }
