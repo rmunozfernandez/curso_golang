5c1dce49636c2f1b6bdb6c05a0c474f8a7c6882e
rmunozfernandez <rmunoz.it@gmail.com>
2022-12-25 22:06:10 -0300

    El uso de los keywords defer, break, continue

--- src/main.go
+++ src/main.go
@@ -3,22 +3,25 @@ package main
 import "fmt"
 
 func main() {
+	// Defer
+	defer fmt.Println("Hola")
+	fmt.Println("mundo")
 
-	switch modulo := 4 % 2; modulo {
-	case 0:
-		fmt.Println("Es par")
-	default:
-		fmt.Println("Es impar")
-	}
+	// Continue y break
+	for i := 0; i < 10; i++ {
 
-	// Sin condición
-	value := 50
-	switch {
-	case value > 100:
-		fmt.Println("Es mayor a 100")
-	case value < 0:
-		fmt.Println("Es menor a 0")
-	default:
-		fmt.Println("No condición")
+		// Continue
+		if i == 2 {
+			fmt.Println("Es 2")
+			continue
+		}
+
+		// Break
+		if i == 8 {
+			fmt.Println("Break")
+			break
+		}
+
+		fmt.Println(i)
 	}
 }
