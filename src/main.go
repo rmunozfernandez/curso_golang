f2103fd66cf5344df86329e8088740be5d9c57e0
rmunozfernandez <rmunoz.it@gmail.com>
2022-12-31 09:33:27 -0300

    Go modules: Ir más alla del GoPath con Echo

--- src/main.go
+++ src/main.go
@@ -1,48 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
-func message(text string, c chan<- string) {
-	c <- text
-}
-
 func main() {
-	c := make(chan string, 2)
-
-	c <- "Mensaje1"
-	c <- "Mensaje2"
-
-	/*
-		len(v Type) int
-		The len built-in function returns the length of v.
-
-		cap(v Type) int
-		The cap built-in function returns the capacity of v.
-	*/
-	fmt.Println(len(c), cap(c))
-
-	close(c)
-
-	//Recorrer un channel
-	for message := range c {
-		fmt.Println(message)
-	}
-
-	//Select
-	email1 := make(chan string, 1)
-	email2 := make(chan string, 1)
-
-	go message("Mensaje 1", email1)
-	go message("Mensaje 2", email2)
-
-	for i := 0; i < 2; i++ {
-		select {
-		case m1 := <-email1:
-			fmt.Println(m1)
-		case m2 := <-email2:
-			fmt.Println(m2)
-		}
-	}
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello world!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
